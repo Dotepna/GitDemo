f8dab8756e90c845cbe8245c7c45463d3f91dfdc
ruslana <dotepna.official@gmail.com>
2020-09-26 21:00:09 +0300

    final commit

---
+++ src/test/java/stepDefinitions/demo2.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+import io.cucumber.java.en.Given;
+
+public class demo2 {
+	
+	@Given("landingpage")
+	public void landingpage() {
+		System.out.println("24251");
+		System.out.println("h7867876a");
+		System.out.println("11111");
+		//afuigha;fg
+		System.out.println("holla111");
+		System.out.println("holla111");
+		System.out.println("holla111");
+	}
+
+	
+	
+}
